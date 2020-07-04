222da69bcaaf2f6165693406303290b859f38edd
ArmenMesropyan <mesropyan200315@gmail.com>
2020-07-04 23:45:25 +0400

    User country received by ip

--- src/js/view/countrySelect.js
+++ src/js/view/countrySelect.js
@@ -1,17 +1,20 @@
 import { generateMaterialize } from '../plugins';
 
 // eslint-disable-next-line consistent-return
-export default function generateCountries(countries, selector) {
+export default async function generateCountries(countries, selector) {
     try {
         const container = document.querySelector(selector);
+        const user = await fetch('https://ipapi.co/json/').then((resp) => resp.json());
         const html = countries.map(({ alpha2Code, flag, name }) => `
-            <option value="${alpha2Code}" data-icon="${flag}" class="countries-select__option">
+            <option value="${alpha2Code}" data-icon="${flag}" ${alpha2Code === user.country ? 'selected disabled' : ''} class="countries-select__option">
                 ${name}
             </option>
         `);
         container.insertAdjacentHTML('afterbegin', html);
 
         generateMaterialize();
+
+        return user;
     } catch (error) {
         return Promise.reject(error);
     }
