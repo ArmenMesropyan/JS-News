107877c3811845b6e2398b342e6003cda6eecc74
ArmenMesropyan <mesropyan200315@gmail.com>
2020-07-07 14:34:15 +0400

    Firebase settings and config added

---
+++ src/js/services/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyBxWcp9x4iueF4VJ8l1xn0EzPfqgIXUW_M',
+    authDomain: 'js-news-9a935.firebaseapp.com',
+    databaseURL: 'https://js-news-9a935.firebaseio.com',
+    projectId: 'js-news-9a935',
+    storageBucket: 'js-news-9a935.appspot.com',
+    messagingSenderId: '15214305152',
+    appId: '1:15214305152:web:bd07ba7f275295d4b46b65',
+};
+
+// eslint-disable-next-line no-undef
+firebase.initializeApp(firebaseConfig);
